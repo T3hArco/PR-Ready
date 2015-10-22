f0ec9de96bebc1f060559d67e345d4afda55ae2e
Arnaud Coel <me@arco.me>
2015-10-22 19:27:08 +0200

    Eerste voorbeeld persistent database

--- src/main/java/be/ehb/swp2/Runner.java
+++ src/main/java/be/ehb/swp2/Runner.java
@@ -1,13 +1,29 @@
 package be.ehb.swp2;
 
+import be.ehb.swp2.manager.UserManager;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import javax.swing.*;
 
 /**
  * Created by arnaudcoel on 22/10/15.
  */
 public class Runner {
+    private static SessionFactory factory;
+
     public static void main(String[] args) {
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        } catch(Throwable e) {
+            System.err.println("Failed to instantiate database: " + e);
+            throw new ExceptionInInitializerError(e);
+        }
 
+        UserManager um = new UserManager(factory);
+        Integer u1 = um.addUser("Arnaud", "password");
+        Integer u2 = um.addUser("Domien", "wachtwoord");
 
+        um.listEmployeesToConsole();
     }
 }
