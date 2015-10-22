7de760a80a838524b2e100b1b6fe65adad14e34b
Arnaud Coel <me@arco.me>
2015-10-22 20:39:42 +0200

    Implemented roles - testing Jenkins

--- src/main/java/be/ehb/swp2/entity/User.java
+++ src/main/java/be/ehb/swp2/entity/User.java
@@ -1,16 +1,29 @@
 package be.ehb.swp2.entity;
 
+import org.hibernate.type.EnumType;
+
+import javax.persistence.Enumerated;
+
+
 /**
  * Created by arnaudcoel on 22/10/15.
  */
 public class User {
     private int id;
     private String name, password;
+    private UserRole userRole;
 
     public User() {}; // default constructor
     public User(String name, String password) {
         this.name = name;
         this.password = password;
+        this.userRole = UserRole.USER;
+    }
+
+    public User(String name, String password, UserRole userRole) {
+        this.name = name;
+        this.password = password;
+        this.userRole = userRole;
     }
 
     public int getId() {
@@ -31,6 +44,14 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public UserRole getUserRole() {
+        return this.userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
     }
 
     public void setPassword(String password) {
---
+++ src/main/java/be/ehb/swp2/entity/UserRole.java
@@ -0,0 +1,12 @@
+package be.ehb.swp2.entity;
+
+/**
+ * Created by arnaudcoel on 22/10/15.
+ */
+
+/**
+ * Hier definieren we de verschillende rollen die een gebruiker kan hebben.
+ */
+public enum UserRole {
+    USER, ADMINISTRATOR
+}
