cdc2efc466248c8180fb5b157da823a06d1bbafa
Arnaud Coel <me@arco.me>
2015-11-09 12:15:48 +0100

    Fixed merge gone bad

--- src/main/java/be/ehb/swp2/ui/LoginWindow.java
+++ src/main/java/be/ehb/swp2/ui/LoginWindow.java
@@ -4,13 +4,8 @@ import be.ehb.swp2.entity.User;
 import be.ehb.swp2.exception.BadLoginException;
 import be.ehb.swp2.exception.DuplicateUserException;
 import be.ehb.swp2.manager.LoginManager;
-import be.ehb.swp2.manager.SessionManager;
 import be.ehb.swp2.manager.UserManager;
-<<<<<<< HEAD
 import be.ehb.swp2.util.Configurator;
-=======
->>>>>>> origin/master
-import com.sun.codemodel.internal.JOp;
 import org.hibernate.SessionFactory;
 
 import javax.swing.*;
