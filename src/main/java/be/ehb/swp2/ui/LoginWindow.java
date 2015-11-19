f6b8bc1063573edf96364e12e33ca208a8679b89
Arnaud Coel <me@arco.me>
2015-11-19 10:52:35 +0100

    Implementatie gedaan van authenticatie in JxBrowser, ook de oude Swing UI's deprecated.

--- src/main/java/be/ehb/swp2/ui/LoginWindow.java
+++ src/main/java/be/ehb/swp2/ui/LoginWindow.java
@@ -3,29 +3,48 @@ package be.ehb.swp2.ui;
 import be.ehb.swp2.entity.User;
 import be.ehb.swp2.exception.BadLoginException;
 import be.ehb.swp2.exception.DuplicateUserException;
+import be.ehb.swp2.exception.UserNotFoundException;
 import be.ehb.swp2.manager.LoginManager;
-import be.ehb.swp2.manager.SessionManager;
 import be.ehb.swp2.manager.UserManager;
 import be.ehb.swp2.util.Configurator;
-import com.sun.codemodel.internal.JOp;
+import com.teamdev.jxbrowser.chromium.*;
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
 import org.hibernate.SessionFactory;
+
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
- * Created by arnaudcoel on 23/10/15.
+ * Created by domienhennion on 9/11/15.
+ * Modified by arnaudcoel 19/11/15 -> implemented window fully.
  */
 
 /**
- * The main login window for PR-Ready
+ * This class provides the implementation of the Login UI using JxBrowser
+ * @implements Window
+ * @extends JFrame
  */
 public class LoginWindow extends JFrame implements Window {
+    /**
+     * Provides a connection to the database
+     */
     private SessionFactory factory;
+
+    /**
+     * Provides a method for the session data of the user to be saved
+     */
     private Configurator configurator;
 
+    /**
+     * Main constructor of the login window. Initializes the variables and then initializes the form
+     * @param factory SQL session
+     */
     public LoginWindow(SessionFactory factory) {
         this.factory = factory;
         this.configurator = new Configurator();
@@ -33,66 +52,111 @@ public class LoginWindow extends JFrame implements Window {
         this.initComponents();
     }
 
+    /**
+     * Initializes the components
+     */
     public void initComponents() {
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        final JTextField username = new JTextField(10);
-        JLabel usernameLabel = new JLabel("Username");
-        usernameLabel.setLabelFor(username);
+        User u = action(this);
+    }
 
-        final JPasswordField password = new JPasswordField(10);
-        JLabel passwordLabel = new JLabel("Password");
-        passwordLabel.setLabelFor(password);
+    /**
+     * The action listener for this form. This will listen to login and registration requests.
+     * @todo the error messages are currently shown with messagedialogs, please put this inside of the HTML
+     * @param parent the parent frame in order to get the data required
+     * @return User object that has been signed in or created.
+     */
+    private User action(JFrame parent) {
+        /**
+         * The result of the calls from JxBrowser
+         */
+        final AtomicReference<User> result = new AtomicReference<User>();
 
-        JButton ok = new JButton("Ok");
-        JButton cancel = new JButton("Cancel aka create");
+        /**
+         * The main 'login' dialog
+         */
+        final JDialog dialog = new JDialog(parent, "Login", true);
 
-        JPanel parent = new JPanel(new GridLayout(3, 2));
-        this.add(parent);
+        /**
+         * The initialized browser using the JxBrowser library
+         */
+        final Browser browser = new Browser();
 
-        parent.add(usernameLabel);
-        parent.add(username);
-        parent.add(passwordLabel);
-        parent.add(password);
-        parent.add(ok);
-        parent.add(cancel);
+        /**
+         * The listener for the register function
+         */
+        browser.registerFunction("onLogin", new BrowserFunction() {
+            public JSValue invoke(JSValue... args) {
+                String username = args[0].getString();
+                String password = args[1].getString();
+                User user = null;
 
-        ok.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent arg0) {
                 LoginManager lm = new LoginManager(factory);
                 UserManager um = new UserManager(factory);
 
                 try {
-                    User user = lm.authenticate(username.getText(), password.getText());
+                    result.set(user = lm.authenticate(username, password));
                     String token = um.setToken(user.getId());
                     configurator.setSetting("user", "token", token);
 
-                    JOptionPane.showMessageDialog(null, "Inloggen is gelukt: " + username.getText() + ", Token: " + token, "PR-Ready", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Inloggen is gelukt: " + username + ", Token: " + token, "PR-Ready", JOptionPane.INFORMATION_MESSAGE);
                 } catch (BadLoginException e) {
                     JOptionPane.showMessageDialog(null, "Gebruikersnaam of wachtwoord incorrect.", "PR-Ready", JOptionPane.ERROR_MESSAGE);
                     e.printStackTrace();
                 }
-            }
 
+                dialog.setVisible(false);
+                return JSValue.createUndefined();
+            }
         });
 
+        browser.registerFunction("onRegister", new BrowserFunction() {
+            public JSValue invoke(JSValue... args) {
+                String username = args[0].getString();
+                String password = args[1].getString();
+                User user = null;
 
-        cancel.addActionListener(new ActionListener() {
+                UserManager um = new UserManager(factory);
 
-            public void actionPerformed(ActionEvent e) {
                 try {
-                    UserManager um = new UserManager(factory);
-                    um.addUser(username.getText(), password.getText());
-                    JOptionPane.showMessageDialog(null, "Gebruiker is aangemaakt", "PR-Ready", JOptionPane.INFORMATION_MESSAGE);
-                } catch (DuplicateUserException e1) {
-                    JOptionPane.showMessageDialog(null, "De gebruiker die u probeert aan te maken bestaat al!", "PR-Ready", JOptionPane.ERROR_MESSAGE);
-                    e1.printStackTrace();
-                }
-            }
+                    Integer userId = um.addUser(username, password);
+                    user = um.getUserById(userId);
 
+                    result.set(user);
+                } catch (DuplicateUserException e) {
+                    JOptionPane.showMessageDialog(null, "De gebruikersnaam die u probeert te gebruiken is al in gebruik!", "PR-Ready", JOptionPane.ERROR_MESSAGE);
+                    e.printStackTrace();
+                } catch (UserNotFoundException e) {
+                    JOptionPane.showMessageDialog(null, "De gebruiker die aangemaakt werd in de databank kon niet teruggevonden worden!", "PR-Ready", JOptionPane.ERROR_MESSAGE);
+                    e.printStackTrace();
+                }
+
+                return JSValue.createUndefined();
+            }
         });
 
-        this.pack();
-        this.setVisible(true);
+        /**
+         * This performs randomization for the request URI. The library we're using has agressive caching, adding a secure random will defeat this problem.
+         */
+        SecureRandom random = new SecureRandom();
+
+        browser.loadURL("http://dtprojecten.ehb.be/~PR-Ready/?" + new BigInteger(130, random));
+        dialog.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                browser.dispose();
+                dialog.setVisible(false);
+                dialog.dispose();
+            }
+        });
+
+        dialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        dialog.add(new BrowserView(browser), BorderLayout.CENTER);
+        dialog.setResizable(false);
+        dialog.setUndecorated(true);
+        dialog.setBounds(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds());
+        dialog.setLocationRelativeTo(parent);
+        dialog.setVisible(true);
+        return result.get();
     }
+
 }
