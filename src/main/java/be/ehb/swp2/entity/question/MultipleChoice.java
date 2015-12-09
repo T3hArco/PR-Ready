a2a3db982a43795ec4778ec24df6e10227eda2b3
CauwenberghThomas <cauwenbergh.thomas@outlook.com>
2015-12-10 00:05:00 +0100

    Merge branch 'master' of https://github.com/T3hArco/PR-Ready
    Conflicts:
    	src/main/java/be/ehb/swp2/ui/ImageWindow.java

--- src/main/java/be/ehb/swp2/entity/question/MultipleChoice.java
+++ src/main/java/be/ehb/swp2/entity/question/MultipleChoice.java
@@ -1,6 +1,7 @@
 package be.ehb.swp2.entity.question;
 
 import be.ehb.swp2.entity.Question;
+import be.ehb.swp2.entity.QuestionType;
 
 /**
  * Created by Thomas on 29/10/2015.
@@ -10,6 +11,9 @@ import be.ehb.swp2.entity.Question;
  * @Todo implement this class
  */
 public class MultipleChoice extends Question {
+    public MultipleChoice(String title, String text, QuestionType questionType, int questionExtraId, String answer) {
+        super(title, text, questionType, questionExtraId);
+    }
 
     public boolean solve() {
         return false;
