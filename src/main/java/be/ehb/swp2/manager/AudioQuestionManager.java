56cc96546a48e663972b3f08e2c604661519925e
Ibrahim Elio <Ibrahim Elio>
2015-12-13 21:46:15 +0100

    AudioQuestionManager : add, delete, update

---
+++ src/main/java/be/ehb/swp2/manager/AudioQuestionManager.java
@@ -0,0 +1,85 @@
+package be.ehb.swp2.manager;
+import be.ehb.swp2.entity.question.AudioQuestion;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ * Created by Ibrahim on 10-12-15.
+ */
+public class AudioQuestionManager {
+    public SessionFactory factory;
+
+    public AudioQuestionManager(SessionFactory factory) {this.factory = factory;}
+   // public AudioQuestionManager(){};
+
+    public Integer addAudioQuestion( int parentQuestion, String link) {
+
+        Session session = factory.openSession();
+        Transaction transaction = null;
+        Integer Id = null;
+
+        try {
+            transaction = session.beginTransaction();
+            AudioQuestion audioquestion = new AudioQuestion(parentQuestion, link);
+            Id = (Integer) session.save(audioquestion);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null)
+                transaction.rollback();
+        } finally {
+            session.close();
+        }
+        return Id;
+    }
+
+    public void deleteAudioQuestion(Integer Id) {
+        Session session = factory.openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            AudioQuestion audioquestion = session.get(AudioQuestion.class, Id);
+            session.delete(audioquestion);
+            transaction.commit(); //
+        } catch (HibernateException e) {
+            if (transaction != null)
+                transaction.rollback();
+
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+
+    public void updateAudioQuestion( int Id, String link, int parentQuestion) {
+        Session session = factory.openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            AudioQuestion audioquestion = session.get(AudioQuestion.class,Id);
+            audioquestion.setLink(link);
+            audioquestion.setParentQuestion(parentQuestion);
+            session.update(audioquestion);
+            transaction.commit();
+        } catch (HibernateException e) {
+
+            if (transaction != null)
+                transaction.rollback();
+
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+
+   /* public static void main (String [] args)
+    {
+
+
+    }*/
+}
